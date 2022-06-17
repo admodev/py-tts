d52cab0cc2042d446c55c4e8d215188968cc035a
admodev <admodevcodes@outlook.com>
2022-06-17 16:47:16 -0300

    add conditional

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import gtts
 from playsound import playsound
 
 # List supported languages FOR TEST PURPOSES ONLY
-# print(gtts.lang.tts_langs())
+print(gtts.lang.tts_langs())
 
 print("Indica lo que deseas decir a continuacion: ")
 
 text = str(input())
+
+if (len(text) == 0):
+    text = "Por favor, escriba algo..."
 
 # make request to google to get speech synthesis
 tts = gtts.gTTS(text, lang="es")
